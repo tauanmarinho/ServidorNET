0fcda4a2605ab0e9e701264e2d4a7380c8205698
Tauan Marinho <tauamarinho@gmail.com>
2018-06-05 21:19:33 -0300

    Adicionando Language sem bd e tasks

---
+++ src/GitBuild.Web/Controllers/LanguageController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GitBuild.Web.Controllers
+{
+    public class LanguageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreateOrEdit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateOrEdit(int id)
+        {
+            return View();
+        }
+        
+    }
+}
